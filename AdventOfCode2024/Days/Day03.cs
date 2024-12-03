b20ab9a1a35ae49819605007e0d8d9f2dd94bfbe
Simon Driendl <s.driendl@metalldeutsch.com>
2024-12-03 09:19:27 +0100

    use named capture groups and simplify computation

--- AdventOfCode2024/Days/Day03.cs
+++ AdventOfCode2024/Days/Day03.cs
@@ -2,9 +2,9 @@
 
 namespace AdventOfCode2024.Days;
 
-public class Day03 : CustomInputPathBaseDay
+public sealed class Day03 : CustomInputPathBaseDay
 {
-    private string _input;
+    private string _input = string.Empty;
     public Day03()
     {
         Initialize();
@@ -17,52 +17,42 @@ public class Day03 : CustomInputPathBaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        List<(int, int)> numbers = [];
-        var rx = new Regex(@"mul\((\d+),(\d+)\)");
+        var rx = new Regex(@"mul\((?<FirstNumber>\d+),(?<SecondNumber>\d+)\)");
         var matches = rx.Matches(_input);
-        
-        foreach(Match match in matches)
-        {
-            var groups = match.Groups;
-            numbers.Add((int.Parse(groups[1].Value), 
-                         int.Parse(groups[2].Value)));
-        }
-        
-        var result = numbers.Sum(t => t.Item1 * t.Item2).ToString();
-        return new ValueTask<string>(result);
+
+        var result = matches.Sum(m =>
+                      int.Parse(m.Groups["FirstNumber"].Value) *
+                      int.Parse(m.Groups["SecondNumber"].Value));
+
+        return new ValueTask<string>(result.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        List<(int, int)> numbers = [];
-        var rx = new Regex(@"(mul\((\d+),(\d+)\))|do\(\)|don't\(\)");
+        var rx = new Regex(@"(mul\((?<FirstNumber>\d+),(?<SecondNumber>\d+)\))|do\(\)|don't\(\)");
         var matches = rx.Matches(_input);
 
         var enabled = true;
-        foreach(Match match in matches)
+        var result = 0;
+        foreach (Match match in matches)
         {
             var groups = match.Groups;
             if (groups[0].Value == "don't()")
             {
                 enabled = false;
-                continue;
             }
-
-            if (groups[0].Value == "do()")
+            else if (groups[0].Value == "do()")
             {
                 enabled = true;
-                continue;
             }
-
-            if (enabled)
+            else if (enabled)
             {
-                numbers.Add((int.Parse(groups[2].Value), 
-                             int.Parse(groups[3].Value)));
+                result += int.Parse(groups["FirstNumber"].Value) *
+                          int.Parse(groups["SecondNumber"].Value);
             }
         }
 
-        var result = numbers.Sum(t => t.Item1 * t.Item2).ToString();
-        return new ValueTask<string>(result);
+        return new ValueTask<string>(result.ToString());
     }
 
 }
