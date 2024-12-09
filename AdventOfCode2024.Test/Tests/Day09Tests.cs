9c85d4fa872113bc6864b9a62643e3c35d651e87
Simon Driendl <s.driendl@metalldeutsch.com>
2024-12-09 11:48:07 +0100

    Day 9

---
+++ AdventOfCode2024.Test/Tests/Day09Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Test.Tests;
+
+public class Day09Tests
+{
+    [Fact]
+    public async void Solve_1_ReturnsCorrectResult()
+    {
+        // Arrange
+        var testInput =
+            """
+            2333133121414131402
+            """;
+
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day09
+        {
+            TestInputFilePath = tempFile.FilePath
+        };
+
+        // Act
+        var result = await day.Solve_1();
+
+        // Assert
+        Assert.Equal("1928", result);
+    }
+    [Fact]
+    public async void Solve_2_ReturnsCorrectResult()
+    {
+        // Arrange
+        var testInput =
+            """
+            2333133121414131402
+            """;
+
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day09
+        {
+            TestInputFilePath = tempFile.FilePath
+        };
+
+        // Act
+        var result = await day.Solve_2();
+
+        // Assert
+        Assert.Equal("2858", result);
+    }
+}
---
+++ AdventOfCode2024/Days/Day09.cs
@@ -0,0 +1,138 @@
+﻿using AdventOfCode.Common;
+using MoreLinq;
+
+namespace AdventOfCode2024.Days;
+
+public sealed class Day09 : CustomInputPathBaseDay
+{
+    private List<int> _input;
+    public Day09()
+    {
+        Initialize();
+    }
+    protected override void Initialize()
+    {
+        _input = [];
+        var input = File.ReadAllText(InputFilePath);//.ToList().Select(c => int.Parse(c.ToString())).ToList();
+        foreach (var n in input)
+        {
+            _input.Add(n - '0');
+        }
+    }
+
+    public async override ValueTask<string> Solve_1()
+    {
+        var mem = Compact(ExpandLayout(_input));
+
+        return mem
+            .Select((x, i) => x == -1 ? 0L : x * i)
+            .Sum()
+            .ToString();
+    }
+
+    public async override ValueTask<string> Solve_2()
+    {
+        var mem = Defrag(ExpandLayout(_input));
+
+        var result = mem
+            .Select((x, i) => x == -1 ? 0L : (long)x * i)
+            .Sum()
+            .ToString();
+
+        return result;
+    }
+
+    private List<int> Defrag(List<int> input)
+    {
+        var rle = input.RunLengthEncode();
+        var chunks = new List<(int Key, int Size, int StartingIndex)>();
+        var idx = 0;
+        foreach (var e in rle)
+        {
+            if (e.Key != -1)
+            {
+                chunks.Add((e.Key, e.Value, idx));
+            }
+            idx += e.Value;
+        }
+        chunks = chunks.OrderByDescending(x => x.Key).ToList();
+        var firstPossibleFree = new int[chunks.Max(x => x.Size) + 1];
+
+        foreach (var chunk in chunks)
+        {
+            SwapChunk(chunk.Key, chunk.Size, chunk.StartingIndex);
+        }
+
+        void SwapChunk(int key, int size, int startingIndex)
+        {
+            for (int i = firstPossibleFree[size]; i < startingIndex - size + 1; i++)
+            {
+                var fits = true;
+                for (int j = i; j < i + size; j++)
+                {
+                    if (input[j] != -1)
+                    {
+                        fits = false;
+                        break;
+                    }
+                }
+                if (fits)
+                {
+                    for (var k = size; k < firstPossibleFree.Length; k++)
+                        firstPossibleFree[k] = Math.Max(i + size, firstPossibleFree[k]);
+                    for (int k = i; k < i + size; k++)
+                    {
+                        input[k] = key;
+                    }
+                    for (int k = startingIndex; k < startingIndex + size; k++)
+                    {
+                        input[k] = -1;
+                    }
+                    break;
+                }
+            }
+        }
+        return input;
+    }
+
+    private List<int> Compact(List<int> input)
+    {
+        var left = 0;
+        for (int i = input.Count - 1; i >= 0; i--)
+        {
+            if (input[i] == -1) continue;
+            if (i <= left) break;
+            for (int j = left; j < input.Count; j++)
+            {
+                if (input[j] != -1) continue;
+                (input[i], input[j]) = (input[j], input[i]);
+                left = j + 1;
+                break;
+            }
+        }
+        return input;
+    }
+
+    private List<int> ExpandLayout(List<int> input)
+    {
+        List<int> result = [];
+        for (int i = 0; i < input.Count; i++)
+        {
+            if (i % 2 == 0)
+            {
+                for (int j = 0; j < input[i]; j++)
+                {
+                    result.Add(i / 2);
+                }
+            }
+            else
+            {
+                for (int j = 0; j < input[i]; j++)
+                {
+                    result.Add(-1);
+                }
+            }
+        }
+        return result;
+    }
+}
