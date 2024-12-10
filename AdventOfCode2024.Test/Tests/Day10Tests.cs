51fd0c00c71920616b977021cbd33ed5b6453d83
Simon Driendl <s.driendl@metalldeutsch.com>
2024-12-10 10:13:37 +0100

    Day 10

---
+++ AdventOfCode2024.Test/Tests/Day10Tests.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Test.Tests;
+
+public class Day10Tests
+{
+    [Fact]
+    public async void Solve_1_Simple()
+    {
+        // Arrange
+        var testInput =
+            """
+            ...0...
+            ...1...
+            ...2...
+            6543456
+            7.....7
+            8.....8
+            9.....9
+            """;
+
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day10
+        {
+            TestInputFilePath = tempFile.FilePath
+        };
+
+        // Act
+        var result = await day.Solve_1();
+
+        // Assert
+        Assert.Equal("2", result);
+    }
+
+    [Fact]
+    public async void Solve_1_ReturnsCorrectResult()
+    {
+        // Arrange
+        var testInput =
+            """
+            89010123
+            78121874
+            87430965
+            96549874
+            45678903
+            32019012
+            01329801
+            10456732
+            """;
+
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day10
+        {
+            TestInputFilePath = tempFile.FilePath
+        };
+
+        // Act
+        var result = await day.Solve_1();
+
+        // Assert
+        Assert.Equal("36", result);
+    }
+
+    [Fact]
+    public async void Solve_2_Simple_1()
+    {
+        // Arrange
+        var testInput =
+            """
+            ..90..9
+            ...1.98
+            ...2..7
+            6543456
+            765.987
+            876....
+            987....
+            """;
+
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day10
+        {
+            TestInputFilePath = tempFile.FilePath
+        };
+
+        // Act
+        var result = await day.Solve_2();
+
+        // Assert
+        Assert.Equal("13", result);
+    }
+
+    [Fact]
+    public async void Solve_2_Simple_2()
+    {
+        // Arrange
+        var testInput =
+            """
+            012345
+            123456
+            234567
+            345678
+            4.6789
+            56789.
+            """;
+
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day10
+        {
+            TestInputFilePath = tempFile.FilePath
+        };
+
+        // Act
+        var result = await day.Solve_2();
+
+        // Assert
+        Assert.Equal("227", result);
+    }
+
+    [Fact]
+    public async void Solve_2_ReturnsCorrectResult()
+    {
+        // Arrange
+        var testInput =
+            """
+            89010123
+            78121874
+            87430965
+            96549874
+            45678903
+            32019012
+            01329801
+            10456732
+            """;
+
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day10
+        {
+            TestInputFilePath = tempFile.FilePath
+        };
+
+        // Act
+        var result = await day.Solve_2();
+
+        // Assert
+        Assert.Equal("81", result);
+    }
+}
---
+++ AdventOfCode2024/Days/Day10.cs
@@ -0,0 +1,117 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode2024.Days;
+
+public class Day10 : CustomInputPathBaseDay
+{
+    private List<List<int>> _input;
+    public Day10()
+    {
+        Initialize();
+    }
+
+    protected override void Initialize()
+    {
+        _input= File.ReadAllLines(InputFilePath).Select(line => line.Select(n => n - '0').ToList()).ToList();
+    }
+
+    public async override ValueTask<string> Solve_1()
+    {
+        var result = 0;
+        for (int y = 0; y < _input.Count; y++) 
+        {
+            for (int x = 0; x < _input[0].Count; x++)
+            {
+                if (_input[y][x] != 0) continue;
+                result += ReachablePeaksFrom(x, y).Count;
+            }
+        }
+
+        return result.ToString();
+    }
+
+    public async override ValueTask<string> Solve_2()
+    {
+        var result = 0;
+        for (int y = 0; y < _input.Count; y++)
+        {
+            for (int x = 0; x < _input[0].Count; x++)
+            {
+                if (_input[y][x] != 0) continue;
+                result += TrailsStartingAt(x, y).Count;
+            }
+        }
+
+        return result.ToString();
+    }
+
+    private ISet<(int X, int Y)> ReachablePeaksFrom(int x, int y)
+    {
+        var frontier = new Stack<(int X, int Y)>();
+        var peaks = new HashSet<(int X, int Y)>();
+
+        frontier.Push((x, y));
+
+        while (frontier.TryPop(out var p))
+        {
+            if (_input[p.Y][p.X] == 9)
+            {
+                peaks.Add((p.X, p.Y));
+                continue;
+            }
+
+            foreach (var neighbor in Neighbors(p))
+            {
+                frontier.Push(neighbor);
+            }
+        }
+        return peaks;
+    }
+
+
+
+    private List<List<(int X, int Y)>> TrailsStartingAt(int x, int y)
+    {
+        var frontier = new Stack<List<(int X, int Y)>>();
+        var trails = new List<List<(int X, int Y)>>();
+
+        frontier.Push([(x,y)]);
+
+        while (frontier.TryPop(out var p)) 
+        {
+            var w = p.Last();
+
+            if (_input[w.Y][w.X] == 9)
+            {
+                trails.Add(p);
+                continue;
+            }
+
+            foreach (var neighbor in Neighbors(w)) 
+            {
+                frontier.Push([.. p, neighbor]);
+            }
+        }
+
+        return trails;
+    }
+
+    private IEnumerable<(int X, int Y)> Neighbors((int X, int Y) p)
+    {
+        return new (int X, int Y)[]
+        {
+                (p.X + 1, p.Y),
+                (p.X - 1, p.Y),
+                (p.X,  p.Y + 1),
+                (p.X,  p.Y - 1),
+
+        }
+        .Where(InBounds)
+        .Where(x => _input[x.Y][x.X] == _input[p.Y][p.X] + 1);
+    }
+
+    private bool InBounds((int x, int y) p)
+    {
+        return p.x >= 0 && p.y >= 0 && p.x < _input[0].Count && p.y < _input.Count;
+    }
+}
