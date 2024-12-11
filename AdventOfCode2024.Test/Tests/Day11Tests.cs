b559c4b1c04641f11fbf3ab39af589310a5a3eb3
Simon Driendl <s.driendl@metalldeutsch.com>
2024-12-11 09:29:35 +0100

    Day 11

---
+++ AdventOfCode2024.Test/Tests/Day11Tests.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode2024.Test.Tests;
+
+public class Day11Tests
+{
+    [Fact]
+    public async void Solve_1_ReturnsCorrectResult()
+    {
+        // Arrange
+        var testInput =
+            """
+            125 17
+            """;
+
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day11
+        {
+            TestInputFilePath = tempFile.FilePath
+        };
+
+        // Act
+        var result = await day.Solve_1();
+
+        // Assert
+        Assert.Equal("55312", result);
+    }
+}
---
+++ AdventOfCode2024/Days/Day11.cs
@@ -0,0 +1,96 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode2024.Days;
+
+public class Day11 : CustomInputPathBaseDay
+{
+    private List<long> _input;
+    public Day11()
+    {
+        Initialize();
+    }
+
+    protected override void Initialize()
+    {
+        _input = File.ReadAllText(InputFilePath)
+            .Split(" ", options: StringSplitOptions.RemoveEmptyEntries)
+            .Select(long.Parse)
+            .ToList();
+    }
+
+    public async override ValueTask<string> Solve_1()
+    {
+        var s = _input
+            .GroupBy(x => x)
+            .ToDictionary(g => g.Key,
+            g => (long)g.Count());
+        for (int i = 0; i < 25; i++)
+        {
+            var newDict = new Dictionary<long, long>();
+            foreach (var stone in s.Keys)
+            {
+                var n = s[stone];
+                foreach (var newStone in ApplyRule(stone))
+                {
+                    var t = newDict.GetValueOrDefault(newStone, 0);
+                    newDict[newStone] = t + n;
+                }
+            }
+            s = newDict;
+        }
+        return s.Sum(k => k.Value).ToString();
+    }
+
+    public async override ValueTask<string> Solve_2()
+    {
+        var s = _input
+            .GroupBy(x => x)
+            .ToDictionary(g => g.Key,
+            g => (long)g.Count());
+        for (int i = 0; i < 75; i++)
+        {
+            var newDict = new Dictionary<long, long>();
+            foreach (var stone in s.Keys)
+            {
+                var n = s[stone];
+                foreach (var newStone in ApplyRule(stone))
+                {
+                    var t = newDict.GetValueOrDefault(newStone, 0);
+                    newDict[newStone] = t + n;
+                }
+            }
+            s = newDict;
+        }
+        return s.Sum(k => k.Value).ToString();
+    }
+
+    private IEnumerable<long> ApplyRule(long stone)
+    {
+        var stoneString = stone.ToString();
+        var nLength = stoneString.Length;
+        if (stone == 0) return [1];
+        else if (nLength % 2 == 0)
+        {
+            var split = SplitLong(stone);
+            return [split.firstHalf, split.secondHalf];
+        }
+        else
+        {
+            return [stone * 2024];
+        }
+    }
+
+    private static (long firstHalf, long secondHalf) SplitLong(long number)
+    {
+        int numDigits = (int)Math.Floor(Math.Log10(number) + 1);
+
+        int midIndex = numDigits / 2;
+
+        long divisor = (long)Math.Pow(10, midIndex);
+
+        long firstHalf = number / divisor;
+        long secondHalf = number % divisor;
+
+        return (firstHalf, secondHalf);
+    }
+}
