9b384da39b94fdcd76844ec2342f44b461b71bdb
Simon Driendl <s.driendl@metalldeutsch.com>
2024-12-13 10:17:03 +0100

    Day 13

---
+++ AdventOfCode2024.Test/Tests/Day13Tests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Test.Tests;
+
+public class Day13Tests
+{
+    [Fact]
+    public async void Solve_1_ReturnsCorrectResult()
+    {
+        // Arrange
+        var testInput =
+            """
+            Button A: X+94, Y+34
+            Button B: X+22, Y+67
+            Prize: X=8400, Y=5400
+
+            Button A: X+26, Y+66
+            Button B: X+67, Y+21
+            Prize: X=12748, Y=12176
+
+            Button A: X+17, Y+86
+            Button B: X+84, Y+37
+            Prize: X=7870, Y=6450
+
+            Button A: X+69, Y+23
+            Button B: X+27, Y+71
+            Prize: X=18641, Y=10279
+            """;
+
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day13
+        {
+            TestInputFilePath = tempFile.FilePath
+        };
+
+        // Act
+        var result = await day.Solve_1();
+
+        // Assert
+        Assert.Equal("480", result);
+    }
+
+    [Fact]
+    public async void Solve_2_ReturnsCorrectResult()
+    {
+        // Arrange
+        var testInput =
+            """
+            Button A: X+94, Y+34
+            Button B: X+22, Y+67
+            Prize: X=8400, Y=5400
+            
+            Button A: X+26, Y+66
+            Button B: X+67, Y+21
+            Prize: X=12748, Y=12176
+            
+            Button A: X+17, Y+86
+            Button B: X+84, Y+37
+            Prize: X=7870, Y=6450
+            
+            Button A: X+69, Y+23
+            Button B: X+27, Y+71
+            Prize: X=18641, Y=10279
+            """;
+
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day13
+        {
+            TestInputFilePath = tempFile.FilePath
+        };
+
+        // Act
+        var result = await day.Solve_2();
+
+        // Assert
+        Assert.Equal("875318608908", result);
+    }
+}
---
+++ AdventOfCode2024/Days/Day13.cs
@@ -0,0 +1,105 @@
+﻿using AdventOfCode.Common;
+using Sprache;
+
+namespace AdventOfCode2024.Days;
+
+public sealed class Day13 : CustomInputPathBaseDay
+{
+    private List<Machine> _input;
+    public Day13()
+    {
+        Initialize();
+    }
+
+    protected override void Initialize()
+    {
+        _input = File.ReadAllText(InputFilePath)
+            .Split(new[] { "\n\n" }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(section => Machine.MachineParser.Parse(section))
+            .ToList();
+    }
+
+    public async override ValueTask<string> Solve_1()
+    {
+        return _input
+            .Sum(Tokens)
+            .ToString();
+    }
+
+    public async override ValueTask<string> Solve_2()
+    {
+        return _input
+            .Select(machine => machine with
+            {
+                P = machine.P with
+                {
+                    X = machine.P.X + 10000000000000,
+                    Y = machine.P.Y + 10000000000000
+                }
+            })
+            .Sum(Tokens)
+            .ToString();
+    }
+
+    private long Tokens(Machine machine)
+    {
+        var m = new long[,]
+        {
+            { machine.A.X, machine.B.X },
+            { machine.A.Y, machine.B.Y}
+        };
+        var v = new long[]
+        {
+            machine.P.X, machine.P.Y,
+        };
+        var (pa, pb) = Solve(m, v);
+        if (pa * machine.A.X + pb * machine.B.X != machine.P.X
+            || pa * machine.A.Y + pb * machine.B.Y != machine.P.Y)
+        {
+            return 0;
+        }
+        return pa * 3 + pb;
+    }
+
+    private (long pa, long pb) Solve(long[,] m, long[] v)
+    {
+        var det = 1.0 / (m[0, 0] * m[1, 1] - m[0, 1] * m[1, 0]);
+        var invA = new double[,] 
+        { 
+            { det * m[1, 1], -det * m[0, 1] }, 
+            { -det * m[1, 0], det * m[0, 0] } 
+        };
+        var pa = v[0] * invA[0, 0] + v[1] * invA[0, 1];
+        var pb = v[0] * invA[1, 0] + v[1] * invA[1, 1];
+        return ((long)Math.Round(pa), (long)Math.Round(pb));
+    }
+
+    private record Machine(Button A, Button B, Prize P)
+    {
+        public static Parser<Machine> MachineParser
+            => from buttonA in Button.ButtonParser("A").Token()
+               from buttonB in Button.ButtonParser("B").Token()
+               from prize in Prize.PrizeParser.Token()
+               select new Machine(buttonA, buttonB, prize);
+    }
+    private record Button(long X, long Y)
+    {
+        public static Parser<Button> ButtonParser(string Name)
+            => from _ in Parse.String($"Button {Name}: X+")
+               from x in Parse.LetterOrDigit.Many().Text().Select(long.Parse)
+               from comma in Parse.Char(',')
+               from _2 in Parse.String(" Y+")
+               from y in Parse.LetterOrDigit.Many().Text().Select(long.Parse)
+               select new Button(x, y);
+    }
+    private record Prize(long X, long Y)
+    {
+        public static Parser<Prize> PrizeParser
+            => from _ in Parse.String("Prize: X=")
+               from x in Parse.LetterOrDigit.Many().Text().Select(long.Parse)
+               from comma in Parse.Char(',')
+               from _2 in Parse.String(" Y=")
+               from y in Parse.LetterOrDigit.Many().Text().Select(long.Parse)
+               select new Prize(x, y);
+    }
+}
