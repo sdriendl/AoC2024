a94a347eb7cc7c6b366d1ceac3126c110db20f5a
Simon Driendl <s.driendl@metalldeutsch.com>
2024-12-17 16:49:07 +0100

    Day 15 Part 2

--- AdventOfCode2024.Test/Tests/Day15Tests.cs
+++ AdventOfCode2024.Test/Tests/Day15Tests.cs
@@ -80,4 +80,176 @@ public class Day15Tests
         // Assert
         Assert.Equal("10092", result);
     }
+
+    [Fact]
+    public async void Solve_2_ReturnsCorrectResult()
+    {
+        // Arrange
+        var testInput =
+            """
+            ##########
+            #..O..O.O#
+            #......O.#
+            #.OO..O.O#
+            #..O@..O.#
+            #O#..O...#
+            #O..O..O.#
+            #.OO.O.OO#
+            #....O...#
+            ##########
+
+            <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+            vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+            ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+            <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+            ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+            ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+            >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+            <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+            ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+            v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+            """;
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day15
+        {
+            TestInputFilePath = tempFile.FilePath,
+        };
+
+        // Act
+        var result = await day.Solve_2();
+
+        // Assert
+        Assert.Equal("9021", result);
+    }
+
+    [Fact]
+    public async void Solve_2_ReturnsCorrectResult2()
+    {
+        // Arrange
+        var testInput =
+            """
+            ##########
+            #O..O.OO.#
+            #.O......#
+            #......OO#
+            #.O.OO.#O#
+            #.OO.@...#
+            #OOOO..O.#
+            #.O.O....#
+            #..OO###.#
+            ##########
+
+            <v<>v><^><vv<^><<>>v<<v^^^^v^<^^v<>^>^^v<<^v<vv<<<>v<<<<<<>^v>^^>>^v>>^^^><<<>vv^<><<^vv<^v>>vvv<v^^
+            """;
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day15
+        {
+            TestInputFilePath = tempFile.FilePath,
+        };
+
+        // Act
+        var result = await day.Solve_2();
+
+        // Assert
+        Assert.Equal("10577", result);
+    }
+
+    [Fact]
+    public async void Solve_2_ReturnsCorrectResult_Edge_Case_Cone()
+    {
+        // Arrange
+        var testInput =
+            """
+            #######
+            #...#.#
+            #.....#
+            #.....#
+            #.....#
+            #.....#
+            #.OOO@#
+            #.OOO.#
+            #..O..#
+            #.....#
+            #.....#
+            #######
+
+            v<vv<<^^^^^
+            """;
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day15
+        {
+            TestInputFilePath = tempFile.FilePath,
+        };
+
+        // Act
+        var result = await day.Solve_2();
+
+        // Assert
+        Assert.Equal("2339", result);
+    }
+
+    [Fact]
+    public async void Solve_2_ReturnsCorrectResult_Edge_Case_2()
+    {
+        // Arrange
+        var testInput =
+            """
+            #######
+            #.....#
+            #.O.O@#
+            #..O..#
+            #..O..#
+            #.....#
+            #######
+
+            <v<<>vv<^^
+            """;
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day15
+        {
+            TestInputFilePath = tempFile.FilePath,
+        };
+
+        // Act
+        var result = await day.Solve_2();
+
+        // Assert
+        Assert.Equal("822", result);
+    }
+
+    [Fact]
+    public async void Solve_2_ReturnsCorrectResult_Edge_Case_3()
+    {
+        // Arrange
+        var testInput =
+            """
+            ########
+            #......#
+            #OO....#
+            #.O....#
+            #.O....#
+            ##O....#
+            #O..O@.#
+            #......#
+            ########
+
+            <^^<<>^^^<v
+            """;
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day15
+        {
+            TestInputFilePath = tempFile.FilePath,
+        };
+
+        // Act
+        var result = await day.Solve_2();
+
+        // Assert
+        Assert.Equal("2827", result);
+    }
 }
--- AdventOfCode2024/Days/Day15.cs
+++ AdventOfCode2024/Days/Day15.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode.Common;
+using Microsoft.Diagnostics.Runtime;
 
 namespace AdventOfCode2024.Days;
 
@@ -27,6 +28,7 @@ public class Day15 : CustomInputPathBaseDay
 
     public async override ValueTask<string> Solve_1()
     {
+        Initialize();
         (int X, int Y) robot = (0, 0);
         for (int y = 0; y < _map.Count; y++)
         {
@@ -67,9 +69,115 @@ public class Day15 : CustomInputPathBaseDay
         return result.ToString();
     }
 
-    public override ValueTask<string> Solve_2()
+    public async override ValueTask<string> Solve_2()
     {
-        throw new NotImplementedException();
+        Initialize();
+        for (int y = 0; y < _map.Count; y++)
+        {
+            _map[y] = _map[y].SelectMany<char, char>(x => x switch
+            {
+                '#' => "##",
+                'O' => "[]",
+                '.' => "..",
+                '@' => "@."
+            }).ToList();
+        }
+
+        (int X, int Y) robot = (0, 0);
+        for (int y = 0; y < _map.Count; y++)
+        {
+            for (int x = 0; x < _map[y].Count; x++)
+            {
+                if (_map[y][x] == '@')
+                    robot = (x, y);
+
+            }
+        }
+        foreach (var move in _moves)
+        {
+            var dir = move switch
+            {
+                '^' => (0, -1),
+                '<' => (-1, 0),
+                '>' => (1, 0),
+                'v' => (0, 1),
+            };
+            if (TryMoveExpanded(robot, dir, out var nextPos))
+            {
+                robot = nextPos;
+            }
+        }
+
+        var result = 0;
+        for (int y = 0; y < _map.Count; y++)
+        {
+            for (int x = 0; x < _map[y].Count; x++)
+            {
+                if (_map[y][x] == '[')
+                    result += 100 * y + x;
+            }
+        }
+
+
+
+        return result.ToString();
+    }
+
+    private bool TryMoveExpanded((int X, int Y) p, (int X, int Y) dir, out (int X, int Y) nextPos)
+    {
+        nextPos = (p.X + dir.X, p.Y + dir.Y);
+        if (CanMoveExpanded(p, dir))
+        {
+            MoveRecursive(p, dir);
+            return true;
+        }
+        return false;
+    }
+
+    private void MoveRecursive((int X, int Y) p, (int X, int Y) dir)
+    {
+        (int X, int Y) nextPos = (p.X + dir.X, p.Y + dir.Y);
+        if (_map[p.Y][p.X] == '.')
+            return;
+        if (dir.Y == 0)
+        {
+            MoveRecursive(nextPos, dir);
+        }
+        else
+        {
+            if (_map[nextPos.Y][nextPos.X] == '[')
+            {
+                MoveRecursive(nextPos, dir);
+                MoveRecursive((nextPos.X + 1, nextPos.Y), dir);
+            }
+            if (_map[nextPos.Y][nextPos.X] == ']')
+            {
+                MoveRecursive(nextPos, dir);
+                MoveRecursive((nextPos.X - 1, nextPos.Y), dir);
+            }
+        }
+        (_map[p.Y][p.X], _map[nextPos.Y][nextPos.X]) = (_map[nextPos.Y][nextPos.X], _map[p.Y][p.X]);
+    }
+
+    private bool CanMoveExpanded((int X, int Y) p, (int X, int Y) dir)
+    {
+        (int X, int Y) nextPos = (p.X + dir.X, p.Y + dir.Y);
+        if (!InBounds(nextPos)) return false;
+        if (_map[nextPos.Y][nextPos.X] == '.')
+            return true;
+        if (dir.Y == 0)
+        {
+            return CanMoveExpanded(nextPos, dir);
+        }
+        if (_map[nextPos.Y][nextPos.X] == '[')
+        {
+            return CanMoveExpanded(nextPos, dir) && CanMoveExpanded((nextPos.X + 1, nextPos.Y), dir);
+        }
+        if (_map[nextPos.Y][nextPos.X] == ']')
+        {
+            return CanMoveExpanded(nextPos, dir) && CanMoveExpanded((nextPos.X - 1, nextPos.Y), dir);
+        }
+        return false;
     }
 
     private bool TryMove((int X, int Y) p, (int X, int Y) dir, out (int X, int Y) nextPos)
