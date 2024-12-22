cd2bb723e51a49f74c17c8bf00cdaa53ba3f8d54
Simon <driendlsimon@gmail.com>
2024-12-22 13:56:51 +0100

    Day 22

---
+++ AdventOfCode2024.Test/Tests/Day22Tests.cs
@@ -0,0 +1,77 @@
+﻿namespace AdventOfCode2024.Test.Tests;
+
+public class Day22Tests
+{
+    [Fact]
+    public async void Solve_1_ReturnsCorrectResult()
+    {
+        // Arrange
+        var testInput =
+            """
+            1
+            10
+            100
+            2024
+            """;
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day22
+        {
+            TestInputFilePath = tempFile.FilePath,
+        };
+
+        // Act
+        var result = await day.Solve_1();
+
+        // Assert
+        Assert.Equal("37327623", result);
+    }
+
+    [Fact]
+    public async void Solve_2_ReturnsCorrectResult()
+    {
+        // Arrange
+        var testInput =
+            """
+            1
+            2
+            3
+            2024
+            """;
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day22
+        {
+            TestInputFilePath = tempFile.FilePath,
+        };
+
+        // Act
+        var result = await day.Solve_2();
+
+        // Assert
+        Assert.Equal("23", result);
+    }
+    
+    [Fact]
+    public async void Solve_2_SimpleExample_ReturnsCorrectResult()
+    {
+        // Arrange
+        var testInput =
+            """
+            123
+            """;
+
+        using var tempFile = new TemporaryInputFile(testInput);
+        var day = new Day22
+        {
+            TestInputFilePath = tempFile.FilePath,
+            N = 10,
+        };
+
+        // Act
+        var result = await day.Solve_2();
+
+        // Assert
+        Assert.Equal("6", result);
+    }
+}
---
+++ AdventOfCode2024/Days/Day22.cs
@@ -0,0 +1,90 @@
+﻿using AdventOfCode.Common;
+using MoreLinq;
+
+namespace AdventOfCode2024.Days;
+
+public sealed class Day22 : CustomInputPathBaseDay
+{
+    private List<int> _input;
+    public int N { get; set; } = 2000;
+
+    public Day22()
+    {
+        Initialize();
+    }
+
+    protected override void Initialize()
+    {
+        _input = File.ReadAllLines(InputFilePath)
+                     .Select(int.Parse)
+                     .ToList();
+    }
+
+    public override async ValueTask<string> Solve_1()
+    {
+        return _input.Sum(seed => NthSecretNumber(seed, N))
+                     .ToString();
+    }
+
+    public override async ValueTask<string> Solve_2()
+    {
+        return _input.AsParallel()
+                     .SelectMany(s => SellPrices(s, N))
+                     .GroupBy(p => p.Item1)
+                     .Max(g => g.Sum(k => k.Item2))
+                     .ToString();
+    }
+
+    private IEnumerable<((long, long, long, long), long)> SellPrices(int seed, int n)
+    {
+        var seen = new HashSet<(long, long, long, long)>();
+        var secrets = NSecretNumbers(seed, n);
+        foreach (var window in secrets.Window(5))
+        {
+            var deltas =
+                (
+                    window[1] % 10 - window[0] % 10,
+                    window[2] % 10 - window[1] % 10,
+                    window[3] % 10 - window[2] % 10,
+                    window[4] % 10 - window[3] % 10
+                );
+            if (seen.Add(deltas))
+            {
+                yield return (deltas, window[4] % 10);
+            }
+        }
+    }
+
+    private long NextSecretNumber(long prev)
+    {
+        prev ^= prev << 6;
+        prev %= 16777216;
+        prev ^= prev >> 5;
+        prev %= 16777216;
+        prev ^= prev << 11;
+        prev %= 16777216;
+
+        return prev;
+    }
+
+    private IEnumerable<long> NSecretNumbers(long seed, int n)
+    {
+        var next = seed;
+        yield return next;
+        for (int i = 0; i < n; i++)
+        {
+            next = NextSecretNumber(next);
+            yield return next;
+        }
+    }
+
+    private long NthSecretNumber(long seed, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            seed = NextSecretNumber(seed);
+        }
+
+        return seed;
+    }
+}
