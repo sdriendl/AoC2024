f28739c06cc3c1feabce2e32a91b915cf6e9df7e
Simon <driendlsimon@gmail.com>
2024-12-30 14:16:13 +0100

    Day 17

---
+++ AdventOfCode2024.Test/Tests/Day17Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2024.Test.Tests;
+
+public class Day17Tests
+{
+    [Fact]
+    public async void Verify_Part2_Solution()
+    {
+        var day = new Day17();
+
+        var expected = string.Join(",", day.program);
+
+        var regA = await day.Solve_2();
+        var actual = day.Run(long.Parse(regA), 0, 0);
+        
+        Assert.Equal(expected, string.Join(",", actual));
+    }
+}
---
+++ AdventOfCode2024/Days/Day17.cs
@@ -0,0 +1,59 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode2024.Days;
+
+public sealed class Day17 : CustomInputPathBaseDay
+{
+    public int[] program = [2, 4, 1, 2, 7, 5, 4, 7, 1, 3, 5, 5, 0, 3, 3, 0];
+    public Day17()
+    {
+        Initialize();
+    }
+
+    protected override void Initialize()
+    {
+    }
+
+    public override async ValueTask<string> Solve_1()
+    {
+        return string.Join(",", Run(32773, 0, 0));
+    }
+
+    public override async ValueTask<string> Solve_2()
+    {
+        return Quine(0, 0, program).Min().ToString();
+    }
+
+    private IEnumerable<long> Quine(long regA, int n, int[] program)
+    {
+        if (Run(regA, 0, 0).SequenceEqual(program))
+        {
+            yield return regA;
+        }
+
+        if (Run(regA, 0, 0).SequenceEqual(program.TakeLast(n)) || n == 0)
+        {
+            for (var i = 0; i < 8; i++)
+            {
+                foreach (var q in Quine(8 * regA + i, n + 1, program))
+                {
+                    yield return q;
+                }
+            }
+        }
+    }
+
+    public IEnumerable<int> Run(long a, long b, long c)
+    {
+        do
+        {
+            b = a % 8;
+            b ^= 2;
+            c = a / (1 << (int)b);
+            b ^= c;
+            b ^= 3;
+            yield return (int)(b % 8);
+            a /= 1 << 3;
+        } while (a != 0);
+    }
+}
